d588ea0c685c9bbfcd18af101a61aa4589237cf1
SupplySide7 <3kasd45@gmail.com>
2026-06-08 00:10:27 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) { return false; }
+/*
+      std::unordered_set<char> seen;
+        for (char x : s) {
+            if (seen.contains(x)) { continue; }
+
+            int sCount = std::count(s.begin(), s.end(), x);
+            int tCount = std::count(t.begin(), t.end(), x);
+            if (sCount != tCount) { return false; }
+        }
+        return true;
+*/
+        std::unordered_map<char, int> sFreq;
+        std::unordered_map<char, int> tFreq;
+
+        for (int i = 0; i < s.size(); i++) {
+            sFreq[s[i]]++;
+            tFreq[t[i]]++;
+        }
+        return (sFreq == tFreq);
+    }
+};
