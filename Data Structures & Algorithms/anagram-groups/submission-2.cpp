829d49587f5474ed8fb4668cb091eea38ca48534
SupplySide7 <3kasd45@gmail.com>
2026-06-09 22:35:23 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> anagramArray;
+        unordered_map<string, vector<string>> anagramMap;
+
+        for (const string &str : strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            anagramMap[key].push_back(str);
+        }
+        for (pair<const string, vector<string>>& entry : anagramMap) {
+            anagramArray.push_back(entry.second); 
+        }
+        return anagramArray;
+    }
+};
