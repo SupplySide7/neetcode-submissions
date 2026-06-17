68840fefc1681b5d4ee39fb88271e247df42ed91
SupplySide7 <3kasd45@gmail.com>
2026-06-17 19:08:17 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) {return !::isalnum(c);}), s.end());
+        unique_ptr<string> front = make_unique<string>(s);
+        unique_ptr<string> back = make_unique<string>(s);
+        
+        for (int i = 0; i < s.size(); i++) {
+            if(!(s[i] == s[s.size() - 1 - i])) {return false;}
+        }
+        return true;
+    }
+};
