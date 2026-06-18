4309f4cc766352cb49696aed3f0582a34add9ea2
SupplySide7 <3kasd45@gmail.com>
2026-06-18 19:54:59 -0400

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        vector<vector<int>> result;
+        std::sort(nums.begin(), nums.end());
+
+        for (auto outer = nums.begin(); outer != std::prev(nums.end()); outer++) {
+            
+            /* sorted ascending order, so no negative numbers in the input set */
+            if (*outer > 0) {break;}
+            
+            /* check for duplicate sets */
+            if(outer > nums.begin() && *outer == *(std::prev(outer))) {continue;} 
+            
+            auto left = std::next(outer);
+            auto right = std::prev(nums.end());
+
+            while (left < right) {                
+                int sum = *outer + *left + *right;
+
+                if (sum == 0) {
+                    result.push_back(vector<int>{*outer, *left, *right});
+                    ++left;
+                    --right;
+                    
+                    /* prevent processing a duplicate triplet */
+                    while (left < right && (*left == *(std::prev(left)))) {
+                        ++left;
+                    }
+                }
+                else if (sum > 0) {
+                    --right;
+                }
+                else {
+                    ++left;
+                }
+            }
+        }
+        return result;
+    }
+};
