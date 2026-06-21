681985037a1f85326a1a42f591a30e1cefd5c8b1
SupplySide7 <3kasd45@gmail.com>
2026-06-21 00:24:53 -0400

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_buy = prices[0];
+        int profit = 0;
+        int max_profit = 0;
+
+        for (int i = 0; i != prices.size(); i++) {
+            min_buy = std::min(min_buy, prices[i]);
+            profit = std::max(0, prices[i] - min_buy);
+            max_profit = std::max(max_profit, profit);
+        }
+        return max_profit;
+    }
+};
