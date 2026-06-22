d4b453f5e1c22623d997817453332fc7e18f56ca
SupplySide7 <3kasd45@gmail.com>
2026-06-21 22:53:12 -0400

    Add: longest-substring-without-duplicates - submission-26

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-26.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        if (s.empty()) {return 0;}
+
+        unordered_map<char, int> seen;
+        int length = 1;
+        int max_length = 0;
+        int start_index = 0;
+        int end_index = 0;
+
+        while (end_index < s.size()) {
+            char c = s[end_index];
+            auto it = seen.find(c);
+
+            if (it != seen.end()) { //duplicate
+                int duplicate_index = it->second;
+                if (duplicate_index < start_index) {
+                    seen.erase(it);
+                } else {
+                    start_index = duplicate_index + 1;
+                }
+                seen[c] = end_index;
+                ++end_index;
+            } else {
+                seen[c] = end_index;
+                ++end_index;
+            }
+            length = end_index - start_index;
+            max_length = std::max(max_length, length);
+        }
+        return max_length;
+    }
+};
