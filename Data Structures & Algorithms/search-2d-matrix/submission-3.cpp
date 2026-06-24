5d7b069201bea569c82c9c342287845262a2ea1a
SupplySide7 <3kasd45@gmail.com>
2026-06-24 15:09:45 -0400

    Add: search-2d-matrix - submission-3

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = static_cast<int>(matrix.size());
+        int n;
+        if (!matrix.empty()) { n = static_cast<int>(matrix[0].size());}
+        else return false;
+        
+
+        for (int i = 0; i < m; i++) {
+            int left = 0;
+            int right = n - 1;
+            
+            if (target >= matrix[i][left] && target <= matrix[i][right]) {
+                
+                while (left <= right) {
+                    int median = left + (right - left)/2;
+
+                    if (target < matrix[i][median]) {
+                        right = median - 1;
+
+                    } else if(target > matrix[i][median]) {
+                        left = median + 1;
+                    } else {return true;}
+                }
+            }
+        }
+        return false;
+    }
+};
