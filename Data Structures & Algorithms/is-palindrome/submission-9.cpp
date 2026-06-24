d2ac51dc0766364bf5e522dbee16449e9d9f8d84
SupplySide7 <3kasd45@gmail.com>
2026-06-23 22:42:08 -0400

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        transform(s.begin(), s.end(), s.begin(), ::tolower);
+        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) {
+            return !::isalnum(c);}), s.end());
+        
+        for (int i = 0; i < (s.size())/2; i++) {
+            if(!(s[i] == s[s.size() - 1 - i])) {return false;}
+        }
+        return true;
+    }
+};
