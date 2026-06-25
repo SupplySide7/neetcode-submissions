47dbd689a9f8b2bc8ce08e883c7aa46aa2cf72d1
SupplySide7 <3kasd45@gmail.com>
2026-06-24 23:52:24 -0400

    Add: eating-bananas - submission-11

---
+++ Data Structures & Algorithms/eating-bananas/submission-11.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = static_cast<int>(piles.size());
+        int max_pile = 0;
+        int hours = 0;
+        
+        max_pile = *std::max_element(piles.begin(), piles.end());
+    
+        int right = max_pile;
+        int left = 1;
+        int k_min = max_pile;
+
+        while (left <= right) {
+            int k_midpoint = left + (right - left)/2;
+            hours = 0;
+            for (int pile : piles) {
+                hours = hours + (pile + k_midpoint - 1)/k_midpoint;
+            }
+            
+            if(hours > h) { // total hours sum is monotonically decreasing as k increases
+                left = k_midpoint + 1;
+            } else {
+                k_min = std::min(k_min, k_midpoint);
+                right = k_midpoint - 1;
+            }
+        }
+        return k_min;
+    }
+};
