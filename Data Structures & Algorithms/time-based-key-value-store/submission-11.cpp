db3104c35f15135051cbd0a385aacaad84ce2f39
SupplySide7 <3kasd45@gmail.com>
2026-06-26 23:50:58 -0400

    Add: time-based-key-value-store - submission-11

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-11.cpp
@@ -0,0 +1,37 @@
+class TimeMap {
+public:
+    TimeMap() {}
+    
+    void set(string key, string value, int timestamp) {
+        hashmap[key].emplace_back(timestamp, value);
+    }
+    
+    string get(string key, int timestamp) {
+        vector<pair<int, string>> &found = hashmap[key];
+        int n = found.size() - 1;
+
+        /*if (timestamp >= (found[n].first)) {
+            return found[n].second;
+        }*/
+
+        int left = 0;
+        int right = n;
+        string result = "";
+
+        while (left <= right) {
+            int midpoint = left + (right - left)/2;
+
+            if (found[midpoint].first > timestamp) {
+                right = midpoint - 1;
+            }
+            else if (found[midpoint].first <= timestamp) {
+                result = found[midpoint].second;
+                left = midpoint + 1;
+            }
+        }
+        return result;
+    }
+
+private:
+    unordered_map<string, vector<pair<int, string>>> hashmap;
+};
